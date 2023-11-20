7f34990c4d00520e040e3c1caf4ffa5b63242343
Me1Ssss <aydar20050807@mail.ru>
2023-11-20 17:06:43 +0300

    Update pract1.py

--- pract1.py
+++ pract1.py
@@ -14,8 +14,6 @@ while score == 1:
     if ans2 == q_ans2:
         print('Верно ,вы получаете очко!')
         score += 1
-    exit
-print()
 while score == 2:
     print('Сколько значений есть у bool?')
     q_ans3 = '2'
@@ -23,8 +21,6 @@ while score == 2:
     if ans3 == q_ans3:
         print('Верно ,вы получаете очко!')
         score += 1
-    exit
-print()
 while score == 3:
     print('Набор рекомендаций,о том,как сделать код Python более читабельным это ...')
     q_ans4 = 'pep8'
@@ -32,8 +28,6 @@ while score == 3:
     if ans4 == q_ans4:
         print('Верно ,вы получаете очко!')
         score += 1
-    exit
-print()
 while score == 4:
     print('Python — это ... язык. Заполните пропуск')
     q_ans5 = 'интерпретируемый'
@@ -41,8 +35,6 @@ while score == 4:
     if ans5 == q_ans5:
         print('Верно ,вы получаете очко!')
         score += 1
-    exit
-print()
 while score == 5:
     print('Какой тип данных:List,Dict,Sets?')
     q_ans6 = 'мутируемый'
@@ -50,8 +42,6 @@ while score == 5:
     if ans6 == q_ans6:
         print('Верно ,вы получаете очко!')
         score += 1
-    exit
-print()
 while score == 6:
     print('Какой тип данных:strings,tuples,numbers?')
     q_ans7 = 'неизменный'
@@ -59,8 +49,6 @@ while score == 6:
     if ans7 == q_ans7:
         print('Верно ,вы получаете очко!')
         score += 1
-    exit
-print()
 while score == 7:
     print('Анонимная функция с одним выражением, часто используемая в качестве inline функции это...')
     q_ans8 = 'лямбда'
@@ -68,8 +56,6 @@ while score == 7:
     if ans8 == q_ans8:
         print('Верно ,вы получаете очко!')
         score += 1
-    exit
-print()
 while score == 8:
     print('Механизм выбора ряда элементов из типов последовательности, таких как список, кортеж, \
           строки и т.д., известен как...(на английском)?')
@@ -78,8 +64,6 @@ while score == 8:
     if ans9 == q_ans9:
         print('Верно ,вы получаете очко!')
         score += 1
-    exit
-print()
 while score == 9:
     print('Специфическое изменение, которое мы делаем в синтаксисе Python, чтобы легко изменять функции это...')
     q_ans10 = 'декоратор'
@@ -87,6 +71,4 @@ while score == 9:
     if ans10 == q_ans10:
         print('Верно ,вы получаете очко!')
         score += 1
-    exit
-print()
 print('Вы набрали 10/10 очков!')
