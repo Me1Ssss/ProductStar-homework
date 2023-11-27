a721c89c2317f2eaf571d14928964f0602a3c6b8
Me1Ssss <aydar20050807@mail.ru>
2023-11-27 09:43:51 +0300

    Update dz_func.py

--- dz_func.py
+++ dz_func.py
@@ -1,20 +1,15 @@
-d = 0
-
-
-def discriminant(a, b, c):
-    global d
-    d = b**2 - 4 * a * c
-    return d
-
 
 def solve(a, b, c):
-    if d < 0:
+    def discriminant(a, b, c):
+        d = b**2 - 4 * a * c
+        return d
+    if discriminant(a, b, c) < 0:
         print('Нет решений')
-    elif d == 0:
+    elif discriminant(a, b, c) == 0:
         print(f'x =  {- b / 2 / a}')
     else:
-        print(f'x1={(-b + d**0.5) / 2 / a}\
-              x2 = {(+b + d**0.5) / 2 / a}')
+        print(f'x1={(-b + discriminant(a,b,c)**0.5) / 2 / a}\
+              x2 = {(+b + discriminant(a,b,c)**0.5) / 2 / a}')
 
 
-solve(1, -1, 3)
+solve(1, 2, 1)
